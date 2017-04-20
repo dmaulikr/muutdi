00d3ebde36660be40b15247ab197860fac412bc2
Luong Dang Hai <dang.luong@student.tut.fi>
2017-04-20 16:56:25 +0300

    Initial Commit

---
+++ Just Work/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Just Work
+//
+//  Created by Jarvis Luong on 20/04/2017.
+//  Copyright © 2017 Hai Dang Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
