34ea32dcddb052cf8e819018093f41f67bea80ee
Luong Dang Hai <dang.luong@student.tut.fi>
2017-04-20 22:23:55 +0300

    Initial Commit

---
+++ Just Work/Utilities.swift
@@ -0,0 +1,35 @@
+//
+//  Utilities.swift
+//  Just Work
+//
+//  Created by Jarvis Luong on 20/04/2017.
+//  Copyright © 2017 Hai Dang Luong. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func applyGradient(colours: [UIColor]) -> Void {
+        self.applyGradient(colours: colours, locations: nil)
+    }
+    
+    func applyGradient(colours: [UIColor], locations: [NSNumber]?) -> Void {
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.frame = self.bounds
+        gradient.colors = colours.map { $0.cgColor }
+        gradient.locations = locations
+        self.layer.insertSublayer(gradient, at: 0)
+    }
+}
+
+extension UIColor {
+    convenience init(rgb rgbValue: UInt) {
+        self.init(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+}
--- Just Work/ViewController.swift
+++ Just Work/ViewController.swift
@@ -7,19 +7,157 @@
 //
 
 import UIKit
+import CoreLocation
+import KDCircularProgress
+import UserNotifications
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let stressBackground = [UIColor(rgb: 0x757F9A), UIColor(rgb: 0xd7dde8)]
+    let happyBackground = [UIColor(rgb: 0xe53935), UIColor(rgb: 0xe35d5b)]
+    let sadBackground = [UIColor(rgb: 0xDAE2F8), UIColor(rgb: 0xD6A4A4)]
+    let angryBackground = [UIColor(rgb: 0xEB5757), UIColor(rgb: 0x000000)]
+    
+    let boredBackground = [UIColor(rgb: 0x304352), UIColor(rgb: 0xd7d2cc)]
+    let tiredBackground = [UIColor(rgb: 0xe9d362), UIColor(rgb: 0x333333)]
+    
+    static let calmMood = [UIColor(rgb: 0x007991), UIColor(rgb: 0x78FFD6)]
+    static let highMood = [UIColor(rgb: 0xF09819), UIColor(rgb: 0xFF512F)]
+    
+    let detailedLowMood = ["Boredom", "Tiredness"]
+    let detailedHighMood = ["Stress", "Happiness", "Sadness", "Anger"]
+
+    @IBOutlet weak var moodButton1: UIButton!
+    @IBOutlet weak var moodButton2: UIButton!
+    @IBOutlet weak var moodButton3: UIButton!
+    @IBOutlet weak var moodButton4: UIButton!
+    
+    @IBOutlet weak var groupBtn34: UIStackView!
+    
+    @IBOutlet weak var rawMoodStatus: UILabel!
+    
+    var moodProgress: KDCircularProgress!
+    var moodPoint: Double = 70.0
+    var predictedMood = "Stress"
+    
+    var locationManager = CLLocationManager()
+    
+    var moodCircle: KDCircularProgress!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set the styling for the mood progress
+        moodProgress = KDCircularProgress(frame: CGRect(x: 20, y: 80, width: 300, height: 300))
+        moodProgress.angle = moodPoint/100*360
+        moodProgress.startAngle = -90
+        moodProgress.progressThickness = 0.6
+        moodProgress.trackThickness = 0.6
+        moodProgress.clockwise = true
+        moodProgress.glowAmount = 0
+        
+        if (moodPoint < 50) {
+            setLowMood()
+            rawMoodStatus.text = "Down"
+        } else {
+            setHighMood()
+            rawMoodStatus.text = "Aroused"
+        }
+        
+        moodProgress.center = CGPoint(x: view.center.x,y: 230)
+        moodProgress.trackColor = UIColor(red: 255, green: 255, blue: 255, alpha: 0.4)
+        self.view.addSubview(moodProgress)
+        
+        // Register for local notification
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound,.badge], completionHandler: {didAllow, error in})
+        
+        sendNotification()
+        
+        // Location request to send to server
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestLocation()
+    }
+    
+    func setHighMood() {
+        // TODO: set High mood
+        moodProgress.set(colors: UIColor(rgb: 0x16BFFD), UIColor(rgb: 0xCB3066))
+        
+        moodButton1.setTitle(detailedHighMood[0], for: UIControlState.normal)
+        moodButton2.setTitle(detailedHighMood[1], for: UIControlState.normal)
+        moodButton3.setTitle(detailedHighMood[2], for: UIControlState.normal)
+        moodButton4.setTitle(detailedHighMood[3], for: UIControlState.normal)
+        
+        if (predictedMood == "Stress") {
+            moodButton1.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: stressBackground)
+        } else if (predictedMood == "Happiness") {
+            moodButton2.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: happyBackground)
+        } else if (predictedMood == "Sadness") {
+            moodButton3.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: sadBackground)
+        } else {
+            moodButton4.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: angryBackground)
+        }
+    }
+    
+    func setLowMood() {
+        // TODO: set Low mood
+        moodProgress.set(colors: UIColor(rgb: 0x16BFFD), UIColor(rgb: 0xCB3066))
+        moodButton1.setTitle(detailedLowMood[0], for: UIControlState.normal)
+        moodButton2.setTitle(detailedLowMood[1], for: UIControlState.normal)
+        groupBtn34.isHidden = true
+        
+        if (predictedMood == "Boredom") {
+            moodButton1.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: boredBackground)
+        } else {
+            moodButton2.backgroundColor = UIColor.blue
+            self.view.applyGradient(colours: tiredBackground)
+        }
+    }
+    
+    func sendNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "Sample title"
+        content.subtitle = "Sample sumtitle"
+        content.body = "Sample body"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "Sample notification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+    // Boiler plate for the Delegate of CLLocationManager
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Do something
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        // Do something
+    }
+    
+    @IBAction func button1Pressed(_ sender: Any) {
+        if (moodPoint < 50) {
+            
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func button2Pressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func button3Pressed(_ sender: Any) {
+        
     }
 
-
+    @IBAction func button4Pressed(_ sender: Any) {
+        
+    }
 }
-
